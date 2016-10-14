d31fca34dc80fc2d059bdbd21c3ca99cccad3a58
Sunny Townsend <sunnytownsend@gmail.com>
2016-10-14 15:11:00 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rabies-modelling-in-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rabies_modelling_in_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns rabies-modelling-in-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [rabies-modelling-in-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
